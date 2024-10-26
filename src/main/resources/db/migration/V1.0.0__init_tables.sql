9e95c17d83ec4cc33098cd429725e231ead8d3a6
Vache Khachatryan <vache.khachatryan1998@gmail.com>
2024-10-26 10:53:44 +0400

    Modified sql script to use static uuid values

--- src/main/resources/db/migration/V1.0.0__init_tables.sql
+++ src/main/resources/db/migration/V1.0.0__init_tables.sql
@@ -35,15 +35,15 @@ CREATE TABLE IF NOT EXISTS garment_categories (
 
 INSERT INTO garment_categories (id, name)
 VALUES
-    (gen_random_uuid(), 'Jeans'),
-    (gen_random_uuid(), 'Jacket'),
-    (gen_random_uuid(), 'Dress'),
-    (gen_random_uuid(), 'Skirt'),
-    (gen_random_uuid(), 'Shirt'),
-    (gen_random_uuid(), 'Shorts'),
-    (gen_random_uuid(), 'Sweater'),
-    (gen_random_uuid(), 'Coat'),
-    (gen_random_uuid(), 'Blouse');
+    ('c43b2106-efdc-48ac-b923-a2429f5219e3', 'Jeans'),
+    ('7763549b-3ba4-45e1-ad92-e03f2d7cfe9b', 'Jacket'),
+    ('5a1dae44-374e-491a-a0c6-302c760843a7', 'Dress'),
+    ('5b402bcc-20fc-452f-bd79-a4ac407b85b3', 'Skirt'),
+    ('a5ba4e1b-1904-4217-a5b7-d2a46df61992', 'Shirt'),
+    ('cc5f842a-2e6b-44d4-8497-dcd5f312ed72', 'Shorts'),
+    ('9d3f9042-4f67-4d54-aa79-6ddf6754f7f8', 'Sweater'),
+    ('a977abd3-3ab6-4974-8dfe-5c5809e6514c', 'Coat'),
+    ('a14e8870-a541-4fb8-b7fc-e9509e4f1a00', 'Blouse');
 
 CREATE TABLE IF NOT EXISTS garments (
     id UUID PRIMARY KEY,
